bfe44ecefda27a22c3f1e555cd29d76a7d05c17d
Sander Niemeijer <niemeijer@stcorp.nl>
2019-10-14 18:08:37 +0200

    Add conda packages for VISAN beta release

---
+++ visan/README.rst
@@ -0,0 +1,10 @@
+Conda recipe for VISAN
+======================
+
+This is the Conda recipe for `VISAN <http://github.com/stcorp/visan/>`_.
+
+The build scripts support ``linux-32``, ``linux-64``, ``osx-64``, and ``win-64``.
+
+Make sure to add ``stcorp`` as channel because VISAN depends on HARP and CODA.
+
+The generated packages can be found on `Anaconda Cloud <https://anaconda.org/stcorp/visan>`_.
---
+++ visan/build.sh
@@ -0,0 +1,14 @@
+export CMAKE_LIBRARY_PATH=$PREFIX/lib
+
+set
+env
+
+if test $BUILD == "x86_64-conda_cos6-linux-gnu" ; then
+  # Patch the invalid paths to X libraries in VTKTargets.cmake
+  sed -i -e "s:/tmp/build/[^\/]*/vtk_[^\/]*/_build_env/x86_64-conda_cos6-linux-gnu/sysroot/usr/lib64/lib\([^\.]*\).so:\1:g" "$PREFIX/lib/cmake/vtk-8.2/VTKTargets.cmake"
+  cmake -DCMAKE_INSTALL_PREFIX="$PREFIX" -DCMAKE_CXX_FLAGS="-L/usr/lib64 -Wl,-rpath-link,/usr/lib64:/lib64"
+else
+  cmake -DCMAKE_INSTALL_PREFIX="$PREFIX"
+fi
+make VERBOSE=1
+make install
